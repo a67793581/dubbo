af55f38025aa807dc8e67bcf99bbbfbbac34352d
Carlo <284474102@qq.com>
2025-03-11 14:14:36 +0800

    1

--- dubbo-plugin/dubbo-compiler/test2.sh
+++ dubbo-plugin/dubbo-compiler/test2.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-COMPILER_PATH="/Users/admin/code/test/dubbo-1/dubbo-plugin/dubbo-compiler/target"
-NATIVE_COMPILER="${COMPILER_PATH}/dubbo-compiler-3.3.4-SNAPSHOT-osx-aarch_64.exe"
-JAR_COMPILER="${COMPILER_PATH}/dubbo-compiler-3.3.4-SNAPSHOT.jar"
-
-if [ -f "$NATIVE_COMPILER" ]; then
-    COMPILER="$NATIVE_COMPILER"
-elif [ -f "$JAR_COMPILER" ]; then
-    COMPILER="java -jar $JAR_COMPILER"
-else
-    echo "找不到编译器文件"
-    exit 1
-fi
-
-protoc --plugin=protoc-gen-dubbo="$COMPILER" \
+protoc --plugin=protoc-gen-dubbo="/Users/admin/code/test/dubbo-1/dubbo-plugin/dubbo-compiler/target/dubbo-compiler-3.3.4-SNAPSHOT-osx-aarch_64.exe" \
        --dubbo_out=./target/generated-sources \
        --proto_path=. \
        test.proto
